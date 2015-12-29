1188fe3ebd7e354b5249cb27cff58a75f3484a9f
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-12-29 13:59:08 -0200

    Firts commit

---
+++ hello_word.rb
@@ -0,0 +1,8 @@
+# puts -> Print the context and skips a line
+# print -> Just print the context but dont skips a line
+# p -> inspected the element present inside the  context
+
+
+puts "Hello World" 
+print "Hello World"
+p "Hello World"
---
+++ variable_and_attributes.rb
@@ -0,0 +1,27 @@
+## Declare a variable
+# in ruby when you declare a variable make the association between name and value
+
+year = 1995
+puts year 
+
+##end
+
+## gets method
+# gets is a method that allows us to enter a value in a variable in through the keyboard console
+
+puts "What your name?"
+name = gets
+puts "Hello "+name
+
+##end
+
+## Typing
+# Ruby is a strongly typed language and is a language implicitly and dynamically typed implicity typed because the types are inferred by the interpreter need not be declared and dynamically typed because Ruby allows the type f the variable can be changed during program execution
+
+years = "20"
+puts years
+years = 20
+puts years
+
+##end
+
