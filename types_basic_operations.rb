e24e76f995565d7124f173e56e9d03c462bee1dd
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-12-29 19:39:00 -0200

    Types and Basic Operations

---
+++ types_basic_operations.rb
@@ -0,0 +1,29 @@
+# -- Types and Basic Operations
+## In Ruby there are 3 types Numerical basic: Fixnum, Bignum and Float Fixnum is the main entire paragraph numbers kind.
+
+calculate = 3*(2+5)/8
+puts "Result: #{calculate}"
+
+# Example types:
+number1 = 99
+number2 = 9999999999999999999999999999999999999
+number3 = 15.0
+
+
+puts "Return Fixnum: #{number1.class}"
+puts "Return Bignum: #{ number2.class}"
+puts "Return Float: #{number3.class}"
+
+# Operations
+op_sum = 2+2
+op_sub = 5-3
+op_mult = 17*2
+op_div = 14/2
+op_pot = 7**2
+
+#sum
+puts "Sum 2+2 = #{op_sum}"
+puts "Subtraction 5-3 = #{op_sub}"
+puts "Multiplication 17*2 = #{op_mult}"
+puts "Division 14/2 =  #{op_div}"
+puts "Potentiation 7**2 = #{op_pot}"
