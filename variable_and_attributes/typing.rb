960062b478eced52669984790e4b481575b7495e
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-12-30 03:13:38 -0200

    Variables and attributes

---
+++ variable_and_attributes/typing.rb
@@ -0,0 +1,9 @@
+# ---------------Typing-------
+# Ruby is a strongly typed language and is a language implicitly and dynamically typed implicity typed because the types are inferred by the interpreter need not be declared and dynamically typed because Ruby allows the type f the variable can be changed during program execution
+
+
+years = "20"
+puts "Return"
+puts years.class
+years = 20
+puts years.class
