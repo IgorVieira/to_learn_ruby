97fa70df1f425e81b897322c789d0fada8d9220f
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-12-30 03:10:57 -0200

    move or destroy this files for better organization

--- hello_word.rb
+++
@@ -1,8 +0,0 @@
-# puts -> Print the context and skips a line
-# print -> Just print the context but dont skips a line
-# p -> inspected the element present inside the  context
-
-
-puts "Hello World" 
-print "Hello World"
-p "Hello World"
