d21eb5a923f83484d0287c0a9e13b7e669fdb951
Igor Vieira <igorpereiraramosvieira@gmail.com>
2015-12-30 03:13:16 -0200

    Strings ruby
    Other ways to write messages and return the value of the variables.

---
+++ strings_ruby/strings_method_capitalize.rb
@@ -0,0 +1,8 @@
+# -- Method capitalize
+## This method has the function to transform the entire first letter of a string into a capital letter.
+
+flash = "barry"
+puts "My name is #{flash.capitalize} Allen, and I am the faster man alive"
+puts "Barry is in capital letter"
+# ps:We can use a notation next to capitalize method for the change is final next to our method, this annotation is called "Bang", it can be represented with this character "!" but it is recommended moderation in their use.
+
---
+++ strings_ruby/strings_mutability.rb
@@ -0,0 +1,7 @@
+## --Mutability --
+# Another main feature of slinging ruby regarding Strings is its mutability capacity
+#adapt and return a specific value by these same changes.
+
+message = "Morning Thais,"
+message << " how are you?"
+puts message
