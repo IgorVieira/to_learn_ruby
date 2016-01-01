66d46ead24546ab6c527978de568f33fcdf4e202
Igor Vieira <igorpereiraramosvieira@gmail.com>
2016-01-01 10:55:27 -0200

    self method

--- object_orientation/oo_self_method.rb
+++ object_orientation/oo_self_method.rb
@@ -2,4 +2,26 @@
 # Self is a method in which to invoke another method of the same class to 
 # get a particular element or feature present within the same class
 
+class Person
+ def self.count
+  @count ||= 0 
+ end
 
+ def self.count=(increment)
+  @count = increment	
+ end
+
+ def initialize(name)
+   @name = name
+   self.class.count +=1
+ end
+
+ def name 
+  @name
+ end	
+
+end
+
+
+person = Person.new("Woody Allen")
+puts Person.count
